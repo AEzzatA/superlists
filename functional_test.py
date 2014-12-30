b2d768621ca1697878ec6628eb88e4ff09ad3aad
Ahmed Ezzat <AEzzatA@gmail.com>
2014-12-30 14:54:44 +0200

    Front page HTML now generated form a template. plus fixing a typo

--- functional_test.py
+++ functional_test.py
@@ -26,7 +26,7 @@ class NewVisitor(unittest.TestCase):
 		#She is invited to enter an item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
-			inbox.get_attribute('placeholder'),
+			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 			)
 
@@ -35,14 +35,14 @@ class NewVisitor(unittest.TestCase):
 
 		#When she hits enter, the page updates and now the page lists
 		#"1: Buy peacock feathers" as an item in a to-do list
-		inputbox.send_keys(keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
-
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 			)
 
 		# There is still a text box inviting her to add another item. She
